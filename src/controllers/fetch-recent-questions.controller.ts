85214fb07341f3a8b38d935751498cca09437f7e
Thiago <thiagoreis93@gmail.com>
2024-08-29 21:04:53 -0300

    add test e2e config

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+
+    const question = await this.prisma.question.findMany({
+      take: 1,
+      skip: (page - perPage) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return { question }
+  }
+}
