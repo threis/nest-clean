5db507c59342867da8a90285df9c8d271d178b56
Thiago <thiagoreis93@gmail.com>
2024-09-05 21:06:06 -0300

    add register student and authentica student use cases

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -20,7 +20,7 @@ export class Student extends Entity<StudentProps> {
     return this.props.password
   }
 
-  static create(props: StudentProps, id: UniqueEntityID) {
+  static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
 
     return student
